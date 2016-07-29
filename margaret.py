915ab4572253244db183cee197ad56ec1c6ef683
Castle Crawford <nathanscottcrawford@gmail.com>
2016-07-29 12:59:37 -0500

    margaret module complete. both modules output as expected. exercise complete.

--- margaret.py
+++ margaret.py
@@ -1,18 +1,54 @@
 import sys
-import picke
+import pickle
 
 
 class Margaret:
 
+  def __init__(self):
+    self.d = {}
+    try:
+      self.d = self.deserialize()
+    except FileNotFoundError:
+      pass
 
   def prompt(self):
-    note = input("Enter quick note > ")
+    # self.d = {'Margaret': []}
+
+    self.deserialize()
+
+    message = input("<")
+    # try:
+    #   self.d['Margaret'].append(message)
+
+    # except KeyError:
+    #   pass
+    self.d.setdefault("Margaret", []).append(message)
+
+    self.serialize()
+
+    print(self.d)
+
+
 
   def serialize(self):
-    with open('notes.txt', 'wb+') as f:
-      pickle.dump(self.all_notes, f)
+    with open('messages.txt', 'wb+') as f:
+      pickle.dump(self.d, f)
 
-    with open('notes.txt', 'rb') as f:
-      binary = f.read()
 
-    return binary
+  def deserialize(self):
+    try:
+      with open('messages.txt', 'rb+') as f: #rb in read binary
+        self.d = pickle.load(f)
+
+
+    except FileNotFoundError:
+      self.d = {}
+
+      print(self.d)
+
+      return self.d
+
+
+if __name__ == '__main__':
+  marg = Margaret()
+  marg.prompt()
